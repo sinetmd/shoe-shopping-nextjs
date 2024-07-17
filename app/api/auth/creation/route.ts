3af15d9e5fd87414f85e57ba93ef81098ed2d42b
sinetmd <dobremrn12@gmail.com>
2024-07-17 23:41:16 +0300

    fixing 2/2 cache

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,12 +1,14 @@
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 // Get the user session from Kinde and save it to
 // the prisma database (careful! we are using a singleton prisma db!) if there is no such user
 // at the end return client to the index page afterwards
 // we are using the post login redirect url (see the .env file)
 export async function GET() {
+  noStore();
   const { getUser } = getKindeServerSession();
 
   const user = await getUser();
